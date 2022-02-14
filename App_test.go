5af677f4ea2ed6ac5980aba2266461a2ea085e11
Aleks Petrov <aleks0317p@gmail.comgit config --global user.name Aleks>
2022-02-14 12:28:34 +0100

    added test all the function

--- App_test.go
+++ App_test.go
@@ -8,7 +8,36 @@ var x, y, result = 6, 7, 13
 realResult := Plus(x, y)
 
 	if realResult != result {
-		t.Errorf("result %d != check %d", realResult, result)
+	t.Errorf("result %d != check %d", realResult, result)
 	}
 }
-
+func TestMinus(t *testing.T){
+var x, y, result = 7, 6, 1
+	
+realResult := Minus(x, y)
+	
+	if realResult != result {	
+	t.Errorf("result %d != check %d", realResult, result)
+	}
+}
+func TestMult(t *testing.T){
+var x, y, result = 6, 7, 42
+		
+realResult := Mult(x, y)
+		
+	if realResult != result {
+	t.Errorf("result %d != check %d", realResult, result)
+	}
+}		
+func TestDiv(t *testing.T){
+var x, y, result = 54, 6, 9
+	
+realResult := Div(x, y)
+	
+	if realResult == 0 {
+	t.Errorf("realResult: %d, result: %d", realResult, result)
+	}	
+	if realResult != result {
+	t.Errorf("result %d != check %d", realResult, result)
+	}
+}
